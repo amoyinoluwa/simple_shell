056564fbf77d96341f4c59dc9cc6cbba93040f0e
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-08-04 08:48:45 -0700

    --

--- shell.c
+++ shell.c
@@ -1,32 +1,22 @@
 #include "shell.h"
 
+/**
+ * exec - creates a child process in shell
+ * @args: arguments
+ * Return: -1 or nothing
+ */
+
 int exec(char *args)
 {
-}
-
-int main(int argc, char **argv)
-{
 	pid_t child;
-	size_t n;
 	int status;
-	char **env, *buffer;
 
-	if (argc < 1)
-		return (-1);
-
-	env = environ;
-	n = 0;
-	while (1)
-	{
-		printf("$: ");
-		getline(&buffer, &n, stdin);
-	}
 	child = fork();
 
 	if (child == -1)
 	{
 		perror("Process failed");
-		return (-1);
+		exit(-1);
 	}
 	if (child == 0)
 	{
@@ -40,5 +30,36 @@ int main(int argc, char **argv)
 	{
 		wait(&status);
 	}
+}
 
+/**
+ * main - main shell program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, -1 on failure
+ */
+
+int main(int argc, char **argv)
+{
+	size_t n;
+	int status;
+	char **tokens, *buffer;
+
+	if (argc < 1)
+		return (-1);
+
+	n = 0;
+	while (1)
+	{
+		printf("$: ");
+		if (getline(&buffer, &n, stdin) == -1)
+			return (-1);
+		if (strcmp("exit", buffer) == 0)
+		{
+			free(buffer);
+			return (-1);
+		}
+		tokens = tokens(buffer);
+		exec(tokens);
+	}
 }
